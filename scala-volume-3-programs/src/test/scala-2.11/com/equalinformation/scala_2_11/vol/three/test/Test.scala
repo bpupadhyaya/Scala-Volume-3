0f76aaa3697faa2aa584771b4cdcc23807c60515
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2016-06-13 23:47:19 -0700

    Initial project structure.

---
+++ scala-volume-3-programs/src/test/scala-2.11/com/equalinformation/scala_2_11/vol/three/test/Test.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.scala_2_11.vol.three.test
+
+/**
+  * Created by bpupadhyaya on 6/13/16.
+  */
+class Test {
+
+}
---
+++ scala-volume-3-programs/src/test/scala/com/equalinformation/scala/vol/three/test/Test.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.scala.vol.three.test
+
+/**
+  * Created by bpupadhyaya on 6/13/16.
+  */
+class Test {
+
+}
